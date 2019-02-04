605f3acea296725d387692714cdd6506f69c4f76
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2019-02-04 19:48:28 +0200

    Add new solved tasks

---
+++ DataTypes/BooleanVariable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BooleanVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            bool yesOrNo = Convert.ToBoolean(str);
+
+            if (yesOrNo)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ DataTypes/SpecialNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SpecialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int digit = i;
+                int sum = 0;
+
+                while (digit > 0)
+                {
+                    sum += digit % 10;
+                    digit = digit / 10;
+                }
+                bool special = (sum == 5 || sum == 7 || sum == 11);
+
+                Console.WriteLine("{0} -> {1}",i, special);
+
+            }
+
+        }
+    }
+}
